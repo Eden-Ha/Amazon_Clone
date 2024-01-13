474acad7c8eca8a3d244d08d6d373fa0fab6a8d5
Eden-Ha <edenmikael12@gmail.com>
2024-01-13 12:22:28 +0300

    first commit

---
+++ src/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const instance = axios.create({
+  // THE API (cloud function) URL
+  baseURL: "http://127.0.0.1:10000",
+  // baseURL: "https://sa-amazon-backend.cyclic.app",
+});
+
+// const instance = axios.create({
+//   baseURL: '',
+//   // withCredentials: false,
+//   // headers: {
+//   //   'Access-Control-Allow-Origin': '*',
+//   //   'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+//   // },
+// })
+
+export default instance;
